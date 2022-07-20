4e6abf6811448d8780c85fc608389a302c3bbd56
477885555 <dorinaldosa@gmail.com>
2022-07-19 23:24:46 -0300

    extrutura-inicial

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import app from './app';
+
+
+app.get('');
+
+app.post('');
+
+
+
---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type a = {
+   id: string,
+   name: string,
+   price: number,
+   image_url: string
+}
+
+
+
+export type b = {
+   id: number,
+   name: string,
+   email: string,
+   password: string
+}
